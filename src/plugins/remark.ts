c4f94baac139180d109e5d2427ca14157cbaee4f
mimiMonads <mimi@monads.dev>
2024-03-11 08:40:45 +0000

    pushing docs from vixeny

--- src/plugins/remark.ts
+++ src/plugins/remark.ts
@@ -1,7 +1,6 @@
 import { PluginTuple, unified } from "unified";
 import { remarkStaticServer } from "vixeny-prespective";
 
-import rehypeDocument from "rehype-document";
 import rehypeFormat from "rehype-format";
 import rehypeStringify from "rehype-stringify";
 import remarkParse from "remark-parse";
@@ -11,10 +10,8 @@ export default remarkStaticServer(unified)({
   preserveExtension: false,
   uses: [
     [remarkParse] as PluginTuple,
-    [remarkRehype] as unknown as PluginTuple,
-    [rehypeDocument, { title: "👋🌍" }] as unknown as PluginTuple,
+    [remarkRehype , {allowDangerousHtml: true}] as unknown as PluginTuple,
     [rehypeFormat] as unknown as PluginTuple,
-    [rehypeStringify] as unknown as PluginTuple,
-    [remarkRehype] as unknown as PluginTuple,
-  ],
+    [rehypeStringify , {allowDangerousHtml: true}] as unknown as PluginTuple,
+  ] ,
 });
